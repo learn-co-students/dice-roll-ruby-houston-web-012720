213817f2170f1842d591354aa7cce2b28fcc2b00
somaia-khalil <sumaya_khair@live.com>
2020-01-27 17:09:43 -0600

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,11 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-def roll
-  # code goes here
-end
+# def roll
+#   # code goes here
+#   rand(1..6)
+# end
+def roll 
+  array = [1,2,3,4,5,6]
+  array.sample
+end 
